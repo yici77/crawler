fc8fbe170a47b3b6b68fa738090304493454510a
yici77 <phoebewang31@gmail.com>
2024-12-07 17:27:55 +0800

    Update threads.py

--- threads.py
+++ threads.py
@@ -1,21 +1,33 @@
-import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
 import time
+import undetected_chromedriver as uc
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
-driver = uc.Chrome
-driver.get("https://www.threads.net/search?q=周杰倫")
+def write_txt(element):
+    filename = "threads.txt"
+    with open(filename, "a", encoding = "utf-8") as outputfile:
+        return outputfile.write(element)
+
+driver = uc.Chrome()
+driver.get("https://www.threads.net/@twtd003?hl=zh-tw")
 driver.execute_script("window.scrollTo(0, 300);")
-time.sleep(5)
+time.sleep(3)
 
-articles_list = driver.find_element(By.CSS_SELECTOR,"div.x1c1b4dv.x13dflua.x11xpdln").find_elements(By.CSS_SELECTOR, "div.x9f619.x1n2onr6.x1ja2u2z")
-article_url = [i.find_element(By.CSS_SELECTOR,f'a[href*="/post/"]').get_attribute("href") for i in articles_list if re.search(self.pattern,i.text)]
-if article_url:
-	articles = []
-	for url in article_url:
-		driver.get(url)
-		time.sleep(3)
-		article = driver.find_element(By.CLASS_NAME,"x1a6qonq").text
-		articles.append(article)
-	if articles:
-		article = "\n".join(articles).strip("\n")+"：D"
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
+posts = soup.find_all("div",'x78zum5 xdt5ytf')  #先找到存放文章的標籤
+
+for post in posts:
+    for element in post.find_all():
+        if element.name == "time":
+            element = element.get("title")+"\n"
+            write_txt(element)
+        elif element.name == "div" and "x1a6qonq" in element.get("class",[]):
+            element = element.text+"\n"
+            write_txt(element)
+        elif element.name == "picture":
+            element = "img："+element.find("img").get("src")
+            write_txt(element)
+    write_txt("\n==================\n")
 		self.write(article)
