85a1efd861cab907622d7d38d40975d283639d7f
yici77 <phoebewang31@gmail.com>
2024-12-07 18:12:01 +0800

    Update dcard.py

--- dcard.py
+++ dcard.py
@@ -3,41 +3,40 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
-def write_txt(element):
-    filename = "dcard.txt"
-    with open(filename, "a", encoding = "utf-8") as outputfile:
-        return outputfile.write(element)
-
 query = ""   #輸入查詢關鍵字
 forum = ""   #輸入查詢看板
 driver = uc.Chrome()
 driver.get(f"https://www.dcard.tw/search/posts?query={query}&forum={forum}&sort=latest")
 time.sleep(3)
 
-soup = BeautifulSoup(driver.page_source, "html.parser")
-find_path = soup.find_all("h2")
-path_list = ["https://www.dcard.tw"+i.find("a").get("href") for i in find_path]
+find_path = driver.find_elements(By.TAG_NAME, "h2")
+path_list = [i.find_element(By.TAG_NAME, "a").get_attribute("href") for i in find_path]
 
+write_article = []
 for path in path_list:
     driver.get(path)
     article = driver.find_element(By.TAG_NAME, "article")
-  
+
     elements = article.find_elements(By.XPATH,".//h1 | .//span | .//picture | .//time | .//a[@target='_blank']")
     for element in elements:
         if element.tag_name == "h1":
             title = "Title：" + element.text + "\n"
-            write_txt(title)
+            write_article.append(title)
         elif element.tag_name == "time":
             post_time = "post time:" + element.get_attribute("title") + "\n"
-            write_txt(post_time)
+            write_article.append(post_time)
         elif element.tag_name == "span":
             text = element.text + "\n"
-            write_txt(text)
-        elif element.tag_name == "picture":
+            write_article.append(text)
+        elif element.tag_name == "picture" and elements.index(element) !=1 :
             img = "img："+element.find_element(By.TAG_NAME,"img").get_attribute("src") + "\n"
-            write_txt(img)
+            write_article.append(img)
         elif element.tag_name == "a":
             link = "link："+element.get_attribute("href") + "\n"
-            write_txt(link)
-    write_txt("\n==================\n")
+            write_article.append(link)
+    write_article.append("\n==================\n")
 driver.close()
+
+filename = "dcard.txt"
+with open(filename, "w", encoding = "utf-8") as outputfile:
+    outputfile.write("".join(write_article))
